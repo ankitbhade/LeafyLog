3475c2cc2457d9f12d2da4f5fe81feb50a2d2141
aabhade <aabhade@ncsu.edu>
2024-05-15 14:48:45 +0530

    Initial Commit

---
+++ LeafyLog/LeafyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyLogApp.swift
+//  LeafyLog
+//
+//  Created by Ankit on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct LeafyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
